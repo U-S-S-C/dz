ae1eb0006274e69f4cba4c9ca63d88ab09e8ee2f
aboba bebra <nikolajmihajlovskij9@gmail.com>
2023-02-05 17:29:15 +0300

    mistake ispravleno

--- dz2/script.js
+++ dz2/script.js
@@ -5,11 +5,10 @@ let url = 'http://localhost:3000/USERS'
 let getData = async (url) => await (await fetch(url)).json();
 
 let clearList = (selector) => selector.innerHTML = "";
-let addInList = (content, listID) => {
-    let theComment = slcFn(content["comment"]);
+let addInList = (name, theComment, listID) => {
     listID.insertAdjacentHTML(`beforeend`, `
         <li class="el">
-            <h3 class="name">${content["name"]}</h3>
+            <h3 class="name">${name}</h3>
             <p class="comment">${theComment}</p>
         </li>
     `)
@@ -22,8 +21,8 @@ let main = async () => {
     clearList(RIGHTLIST);
     let data = await getData(url);
     for (let i = 0; i<data.length; i++){
-        if(i%2 ==0) addInList(data[i], LEFTLIST);
-        else addInList(data[i], RIGHTLIST);
+        addInList(data[i]["name"], data[i]["comment"], LEFTLIST);
+        addInList(data[i]["name"], slcFn(data[i]["comment"]), RIGHTLIST);
     }
     console.log(data);
 }
