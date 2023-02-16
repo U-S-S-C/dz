38d65ae43a15c615b28d09b17243ecb10b2e53a1
aboba bebra <nikolajmihajlovskij9@gmail.com>
2023-02-16 23:30:42 +0300

    just another commit while im making n5

---
+++ n5/script.js
@@ -0,0 +1,54 @@
+let BLOCKCSS    = document.querySelector('#blockCSS');
+let GETBUTTON   = document.querySelector('#getBlocks');
+let SENDBUTTON  = document.querySelector('#addBlock');
+let SCREEN      = document.querySelector('#screen');
+let URL         = 'http://localhost:3000/USERS';
+
+let getData = async (url) => await (await fetch(url)).json();
+
+let clearList = (selector) => selector.innerHTML = "";
+
+class basedBlock {
+    constructor(style){
+        let blockSelector =
+    }
+    addToScreen(screenSelector){
+        
+    }
+}
+
+// let addInList = (user, listSelector) => {
+//     listSelector.insertAdjacentHTML(`beforeend`, `
+//
+//     `)
+// }
+
+let getBlocks = async () => {
+    clearList(SCREEN);
+    let data = await getData(URL);
+    for (let i = 0; i<data.length; i++)
+        addInList(data[i], SCREEN);
+}
+
+let readUser = () => {
+    return {
+        "id": ID.value,
+        "name": NAME.value,
+        "age": AGE.value,
+        "sex": SEX.value
+    };
+}
+
+let addUser = async () => {
+    const res = await fetch(URL, {
+        method: 'POST',
+        body: JSON.stringify(readUser()),
+        headers: {
+            'Content-type':'application/json; charset=UTF-8'
+        },
+    })
+    return res.json();
+}
+
+GETBUTTON.addEventListener('click', getUsers);
+SENDBUTTON.addEventListener('click', addUser);
