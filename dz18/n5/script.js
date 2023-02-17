74c22712498429b24d4c280650b920ddbecee63d
aboba bebra <nikolajmihajlovskij9@gmail.com>
2023-02-17 21:41:51 +0300

    n5 ready

--- dz18/n5/script.js
+++ dz18/n5/script.js
@@ -2,47 +2,49 @@ let BLOCKCSS    = document.querySelector('#blockCSS');
 let GETBUTTON   = document.querySelector('#getBlocks');
 let SENDBUTTON  = document.querySelector('#addBlock');
 let SCREEN      = document.querySelector('#screen');
-let URL         = 'http://localhost:3000/USERS';
+let URL         = 'http://localhost:3000/BLOCKS';
 
 let getData = async (url) => await (await fetch(url)).json();
 
 let clearList = (selector) => selector.innerHTML = "";
 
 class basedBlock {
+    data;
+    x;
+    y;
+    
     constructor(style){
-        let blockSelector ='';
+        this.data = style;
+        this.x = Math.floor(Math.random() * (document.documentElement.clientWidth-400));
+        this.y = Math.floor(Math.random() * (document.documentElement.clientHeight-500));
     }
-    addToScreen(screenSelector){
-
+    addToScreen = (screenSelector) => {
+        screenSelector.insertAdjacentHTML(`beforeend`, `
+            <div style="position:relative;${this.data} left:${this.x}px; top:${this.y}px;"></div>
+        `)
     }
 }
-
-// let addInList = (user, listSelector) => {
-//     listSelector.insertAdjacentHTML(`beforeend`, `
-//
-//     `)
-// }
 
 let getBlocks = async () => {
     clearList(SCREEN);
     let data = await getData(URL);
-    for (let i = 0; i<data.length; i++)
-        addInList(data[i], SCREEN);
-}
-
-let readUser = () => {
-    return {
-        "id": ID.value,
-        "name": NAME.value,
-        "age": AGE.value,
-        "sex": SEX.value
+    for (let i = 0; i<data.length; i++){
+        let block = new basedBlock(data[i]["style"]);
+        block.addToScreen(SCREEN);
+        console.log(data[i]);
     };
 }
 
-let addUser = async () => {
+let readBlock = () => {
+    return {
+        "style": BLOCKCSS.value
+    };
+}
+
+let addBlock = async () => {
     const res = await fetch(URL, {
         method: 'POST',
-        body: JSON.stringify(readUser()),
+        body: JSON.stringify(readBlock()),
         headers: {
             'Content-type':'application/json; charset=UTF-8'
         },
@@ -50,5 +52,5 @@ let addUser = async () => {
     return res.json();
 }
 
-GETBUTTON.addEventListener('click', getUsers);
-SENDBUTTON.addEventListener('click', addUser);
+GETBUTTON.addEventListener('click', getBlocks);
+SENDBUTTON.addEventListener('click', addBlock);
